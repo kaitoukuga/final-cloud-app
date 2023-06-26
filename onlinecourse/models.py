de87e5027cc2f5c2b8b4fb3accc62ee99fe906b4
kaitoukuga <greaper747@gmail.com>
2023-06-26 06:16:50 -0400

    add new model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -77,7 +77,7 @@ class Lesson(models.Model):
 
 
 # Enrollment model
-# <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
+# Once a user enrolled a class, an enrollment entry should be created between the user and course
 # And we could use the enrollment to track information such as exam submissions
 class Enrollment(models.Model):
     AUDIT = 'audit'
@@ -95,40 +95,48 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
-# <HINT> Create a Question Model with:
-    # Used to persist question content for a course
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
-    # Has a grade point for each question
-    # Has question content
-    # Other fields and methods you would like to design
-#class Question(models.Model):
+# Create a Question Model
+class Question(models.Model):
     # Foreign key to lesson
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     # question text
+    question_text = models.TextField()
     # question grade/mark
+    mark = models.IntegerField()
 
-    # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
+    # A model method to calculate if learner get the score of the question
+    def correct_answer(self, selected_ids):
+       all_answers = self.choice_set.filter(is_correct=True).count()
+       selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+       if all_answers == selected_correct:
+           return True
+       else:
+           return False
+
+    def __str__(self):
+        return self.question_text
 
 
-#  <HINT> Create a Choice Model with:
-    # Used to persist choice content for a question
-    # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
+#  Create a Choice Model
+class Choice(models.Model):
+    # One-To-Many relationship with Question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # Choice content
-    # Indicate if this choice of the question is a correct one or not
-    # Other fields and methods you would like to design
-# class Choice(models.Model):
+    choice_content = models.TextField()
+    # Indicator the correct answer
+    is_correct = models.BooleanField(default=True)
 
-# <HINT> The submission model
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
-#    Other fields and methods you would like to design
+    def __str__(self):
+        return self.choice_content
+
+# Submission model
+class Submission(models.Model):
+   # One enrollment could have multiple submission
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
+   # Time and Date Field
+   date_submitted  = models.DateField(default=now, editable=False)  
+   time = models.TimeField(default=now, editable=False)
+
+    def __str__(self):
+        return f"Submission submitted on {self.date_submitted} at {self.time} for {self.enrollment}"
